2290abbec17965bfcb0d13afeb325f92d1c6af5f
annetboona <annetboona@gmail.com>
2026-05-20 17:04:14 +0200

    Developing Bookings Endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './modules/auth/routes.js'
-
+import housingsRoutes from "./modules/housing/routes.js"
+import bookingRoutes from "./modules/bookings/routes.js"
 dotenv.config()
 
 const app = express()
@@ -9,6 +10,9 @@ const app = express()
 app.use(express.json())
 app.use('/api/auth', authRoutes)
 
+
+app.use("/api/listins", housingsRoutes )
+app.use("/api/bookings", bookingRoutes)
 app.get('/', (req, res) => {
   res.json({ message: 'UniStay+ API is running' })
 })
