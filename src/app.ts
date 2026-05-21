d70ad5b392d7ec27326fe1d3657e40b218e7ddcf
Kevin-webdesign <uzamurerakevin01@gmail.com>
2026-05-21 11:44:46 +0200

    add materials and course modules

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,20 @@
-<<<<<<< HEAD
-import express from 'express'
-import dotenv from 'dotenv'
-import authRoutes from './modules/auth/routes.js'
-import housingsRoutes from "./modules/housing/routes.js"
-import bookingRoutes from "./modules/bookings/routes.js"
-dotenv.config()
-=======
 import "dotenv/config";
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import skillsRoutes from "./routes/skills.routes.js";
-import courseRoutes from "./routes/courec.routes.js";
+import courseRoutes from "./routes/course.routes.js";
+import materialsRoute from "./routes/materials.routes.js"
 
 const app = express();
->>>>>>> origin/main
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/skills", skillsRoutes);
 app.use("/api/courses", courseRoutes);
+app.use("/api/materials", materialsRoute);
 
 app.get("/", (_req, res) => {
   res.json({ message: "UniStay+ API is running" });
 });
 
-<<<<<<< HEAD
-
-app.use("/api/listins", housingsRoutes )
-app.use("/api/bookings", bookingRoutes)
-app.get('/', (req, res) => {
-  res.json({ message: 'UniStay+ API is running' })
-})
-
-export default app
-=======
 export default app;
->>>>>>> origin/main
---
+++ src/routes/course.routes.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  createCourse,
+  deleteCourse,
+  getCourseById,
+  getCourses,
+  publishCourse,
+  updateCourse,
+} from "../controllers/course.controller.js";
+
+
+const router = express.Router();
+
+router.post("/", createCourse);
+router.get("/", getCourses);
+router.get("/:id", getCourseById);
+router.put("/:id",updateCourse);
+router.delete("/:id", deleteCourse);
+router.put("/:id/publish", publishCourse);
+
+export default router;
