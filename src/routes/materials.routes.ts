223258aa48d08af19ba72edea45d752b721c2649
Kevin-webdesign <uzamurerakevin01@gmail.com>
2026-06-02 09:51:34 +0200

    added instructor role

--- src/routes/materials.routes.ts
+++ src/routes/materials.routes.ts
@@ -7,16 +7,16 @@ import {
   updateMaterial,
 } from "../controllers/materials.controller.js";
 
-import { authenticate } from "../middleware/auth.middleware.js";
+import { authenticate, optionalAuthenticate, requireInstructor } from "../middleware/auth.middleware.js";
 
 
 const router = express.Router();
 const auth = authenticate as RequestHandler;
 
-router.post("/course/:courseId", auth, createMaterial);
-router.get("/", getMaterials);
-router.get("/:id", getMaterialById);
-router.put("/:id", auth, updateMaterial);
-router.delete("/:id", auth, deleteMaterial);
+router.post("/course/:courseId", auth, requireInstructor, createMaterial);
+router.get("/", optionalAuthenticate, getMaterials);
+router.get("/:id", optionalAuthenticate, getMaterialById);
+router.put("/:id", auth, requireInstructor, updateMaterial);
+router.delete("/:id", auth, requireInstructor, deleteMaterial);
 
 export default router;
